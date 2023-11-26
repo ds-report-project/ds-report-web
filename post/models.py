a349ff327027e97eb742eda3ba4a3ca3c15a0e8f
mj12130 <jmjj2010@naver.com>
2023-11-26 21:11:30 +0900

    [FEAT] 카테고리 기능_SlugField 만들기 #3

--- post/models.py
+++ post/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
 
     ANONYMOUS = '익명'
