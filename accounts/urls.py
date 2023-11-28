1152a96d343a150ec85b57cbb13c2dfd695b341b
한지수 <eldeoddt@gmail.com>
2023-11-28 21:45:13 +0900

    [FEAT] 로그인, 회원가입, 회원탈퇴 구현 #6

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import *
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('delete_account/', views.delete_account, name='delete_account'),
 ]
--- dsReport/settings.py
+++ dsReport/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'bootstrap4',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
