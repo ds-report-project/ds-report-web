5a0d76c79b0a9fcb22dd39e0876acdd326fb17d9
seunghyeonKang <101852433+seunghyeonKang@users.noreply.github.com>
2023-11-30 22:15:11 +0900

    [Feat] ~검색 기능 완성~ #2

--- post/views.py
+++ post/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.core.exceptions import PermissionDenied
 from django.db import models
+from django.db.models import Q
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 
 
@@ -74,12 +76,17 @@ class PostDelete(DeleteView):
             return super(PostDelete, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
-        
+
 def post_search(request):
-    query = request.GET.get('q')  # 폼에서 전달된 검색어를 가져옴
+    query = request.GET.get('q')  # 검색어 가져오기
+
+    # if len(query) == 1 :
+    #     messages.error(request.request, '검색어는 2글자 이상 입력해주세요.')
     if query:
-        posts = Post.search(query)  # Post 모델의 search 메서드로 검색 실행
+        posts = Post.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        )
     else:
         posts = Post.objects.none()  # 빈 쿼리셋 반환 (검색어가 없는 경우)
 
-    return render(request, 'post/search.html', {'posts': posts, 'query': query})
+    return render(request, 'post/base.html', {'posts': posts, 'query': query})
